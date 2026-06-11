38163f98f041a2d54fa74c5e907a5de8eef9a074
iamcocotheking-cmd <iamcocotheking@gmail.com>
2026-06-11 18:39:59 +0600

    a

---
+++ PojavLauncher-3_openjdk/app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/ui/DurbinMenuCallbacks.kt
@@ -0,0 +1,31 @@
+package net.kdt.pojavlaunch.ui
+
+import com.kdt.mcgui.mcVersionSpinner
+
+/**
+ * Callback bridge from Compose dashboard to existing PojavLauncher Java components.
+ */
+class DurbinMenuCallbacks(
+    val onLaunch: () -> Unit,
+    val onOpenSettings: () -> Unit,
+    val onOpenControls: () -> Unit,
+    val onOpenDirectory: () -> Unit,
+    val onShareLogs: () -> Unit,
+    val onInstallJar: () -> Unit,
+    val onOpenAccounts: () -> Unit,
+    val onOpenVersions: () -> Unit,
+    val onEditProfile: () -> Unit,
+    val onSelectVanilla: () -> Unit,
+    val onSelectFabric: () -> Unit,
+    val onSelectForge: () -> Unit,
+    val onDurbinComingSoon: () -> Unit,
+    val versionSpinner: mcVersionSpinner,
+    val getProfileName: () -> String,
+    val getVersionId: () -> String,
+    val getLoaderLabel: () -> String,
+    val getAccountName: () -> String,
+    val isOfflineAccount: () -> Boolean,
+    val getRamAllocation: () -> String,
+    val getRenderer: () -> String,
+    val getRuntime: () -> String,
+)

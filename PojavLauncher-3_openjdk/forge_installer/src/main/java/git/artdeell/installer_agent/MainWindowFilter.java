7b6f48f6c6a45ba46be0a8a55aac633b34294e8d
iamcocotheking-cmd <iamcocotheking@gmail.com>
2026-06-19 23:30:00 +0600

    cosa

---
+++ PojavLauncher-3_openjdk/forge_installer/src/main/java/git/artdeell/installer_agent/MainWindowFilter.java
@@ -0,0 +1,16 @@
+package git.artdeell.installer_agent;
+
+import java.awt.Component;
+
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+public class MainWindowFilter implements ComponentFilter {
+    @Override
+    public boolean checkComponent(Component component) {
+        return component instanceof JRadioButton
+                || component instanceof JTextField
+                || component instanceof JButton;
+    }
+}
